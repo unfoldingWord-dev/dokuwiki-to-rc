f27cd43e35bd9a02d26cbabcb19ac060d6a476b5
Bruce McLean <mcleanb@photo-nomad.net>
2017-09-04 21:29:42 -0400

    migration_upload.py - working on api calls

--- migration_upload.py
+++ migration_upload.py
@@ -27,7 +27,7 @@ import requests
 import shutil
 from general_tools import file_utils
 
-HOST_NAME = 'https://aws.door43.org/'
+HOST_NAME = 'https://aws.door43.org'
 RETRY_FAILURES = False
 DESTINATION_FOLDER = '../ConvertedDokuWiki'
 access_token = None
@@ -36,8 +36,7 @@ access_token = None
 def get_url(url):
     """
     :param str|unicode url: URL to open
-    :param bool catch_exception: If <True> catches all exceptions and returns <False>
-    :return tuple of file contents and header Link
+    :return response
     """
     headers = {
         'Authorization': 'token ' + access_token
@@ -47,20 +46,59 @@ def get_url(url):
     return response
 
 
+def post_url(url, data):
+    """
+    :param str|unicode url: URL to open
+    :param bool catch_exception: If <True> catches all exceptions and returns <False>
+    :return tuple of file contents and header Link
+    """
+    headers = {
+        'Authorization': 'token ' + access_token,
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+
+    response = requests.post(url, data, headers=headers)
+    return response
+
+
 def upload_repos():
 
-    url = HOST_NAME + 'api/v1/photonomad0/repos'
-    # url = HOST_NAME + 'api/v1/user/repos'
-    # url = HOST_NAME + 'api/v1/repos/search?q=php&uid=0&limit=75'
+    url = HOST_NAME + '/api/v1/photonomad0/repos'
+    # url = HOST_NAME + '/api/v1/user/repos'
+    # url = HOST_NAME + '/api/v1/repos/search?q=php&uid=0&limit=75'
     # response = get_url(url)
 
-    isRepoPresent('photonomad', '')
+    found1 = isRepoPresent('Door43', 'en-obs')
+    found2 = isRepoPresent('Door43', 'en-obs2')
+    found3 = createRepo('Door43', 'en-obs2')
+    found4 = isRepoPresent('Door43', 'en-obs2')
 
 def isRepoPresent(user, repo):
-    url = HOST_NAME + 'api/v1/repos/search?q={0}&uid={1}'.format(repo, user)
+    url = HOST_NAME + '/api/v1/repos/{0}/{1}'.format(user, repo)
     response = get_url(url)
-    list = json.loads(response.text)
-    return list
+    text = response.text
+    if not text:
+        return False
+    dict = json.loads(text)
+    return dict and (len(dict) > 0)
+
+def createRepo(user, repo):
+    """
+    Note that user must be the same as the user for access_token
+    :param user:
+    :param repo:
+    :return:
+    """
+    url = HOST_NAME + '/api/v1/user/repos'
+    data = 'name={0}'.format(repo)
+    response = post_url(url, data)
+    text = response.text
+    if text:
+        dict = json.loads(text)
+        if dict and 'full_name' in dict:
+            full_name = '{0}/{1}'.format(user, repo)
+            return dict['full_name'] == full_name
+    return False
 
 def upload_obs(lang):
     return True
